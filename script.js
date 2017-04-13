45933018111240d4cd84a1e56450f7f3486c6b92
Bryan Falls <bfalls1587@gmail.com>
2017-04-13 13:33:41 -0400

    labs 4 and 5 plus fizzbuzz

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
-function tempConv (temp, unit) {
-	var str = "Takes value of " + temp + unit + " and convert it to the opposite unit."
-	if (unit === "F") {
-	console.log((temp-32)*(5/9) + " C");
-	} else if (unit === "C") {
-	console.log((temp * (9/5) + 32) + " F");
-	}
-	console.log(str);
-}
+// Lab 5
 
-tempConv(100, "C");
+var temp = 100;
+
+console.log(temp * (5/9) + 32 + " C");
+
+
+
+
+
+// Lab 6
+
+// function tempConv (temp, unit) {
+// 	var str = "Takes value of " + temp + unit + " and convert it to the opposite unit."
+// 	if (unit === "F") {
+// 	console.log((temp-32)*(5/9) + " C");
+// 	} else if (unit === "C") {
+// 	console.log((temp * (9/5) + 32) + " F");
+// 	}
+// 	console.log(str);
+// }
+
+// tempConv(212, "C");
+// tempConv(32, "C");
+// tempConv(65, "C");
+// tempConv(100, "F");
+// tempConv(-40, "C");
+
+
+
+//FizzBuzz
+
+// for (var i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0 ){
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
