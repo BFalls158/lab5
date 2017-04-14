fd240603e3548b007a67a1171464fd36b4c88065
Bryan Falls <bfalls1587@gmail.com>
2017-04-14 11:27:32 -0400

    Lab 6 complete

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Lab 5
 
-// var temp = 100;
+var temp = 100;
 
-// console.log(temp * (5/9) + 32 + " C");
+console.log(temp * (5/9) + 32 + " C");
 
 
 
@@ -10,36 +10,36 @@
 
 // Lab 6
 
-// function tempConv (temp, unit) {
-// 	var str = "Takes value of " + temp + unit + " and convert it to the opposite unit."
-// 	if (unit === "F") {
-// 	console.log((temp-32)*(5/9) + " C");
-// 	} else if (unit === "C") {
-// 	console.log((temp * (9/5) + 32) + " F");
-// 	}
-// 	console.log(str);
-// }
+function tempConv (temp, unit) {
+	var str = "Takes value of " + temp + unit + " and convert it to the opposite unit."
+	if (unit === "F") {
+	console.log((temp-32)*(5/9) + " C");
+	} else if (unit === "C") {
+	console.log((temp * (9/5) + 32) + " F");
+	}
+	console.log(str);
+}
 
-// tempConv(212, "C");
-// tempConv(32, "C");
-// tempConv(65, "C");
-// tempConv(100, "F");
-// tempConv(-40, "C");
+tempConv(212, "C");
+tempConv(32, "C");
+tempConv(65, "C");
+tempConv(100, "F");
+tempConv(-40, "C");
 
 
-//FizzBuzz
+// FizzBuzz
 
-// for (var i = 1; i <= 100; i += 1) {
-//   if (i % 3 === 0 && i % 5 === 0 ) {
-//     console.log("FizzBuzz");
-//   } else if (i % 3 === 0) {
-//     console.log("Fizz");
-//   } else if (i % 5 === 0) {
-//     console.log("Buzz");
-//   } else {
-//     console.log(i);
-//   }
-// }
+for (var i = 1; i <= 100; i += 1) {
+  if (i % 3 === 0 && i % 5 === 0 ) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
 
 // What should I wear?
 
@@ -49,7 +49,7 @@ function whatDoIWear (event, temp) {
 		suggestion += "something comfy ";
 	} else if (event === "semi-formal") {
 		suggestion += "a polo ";
-	} else {
+	} else if (event === "formal") {
 		suggestion += "a suit ";
 	}
 
@@ -59,13 +59,12 @@ function whatDoIWear (event, temp) {
 		suggestion += "a coat";
 	} else if (temp >= 54 && temp <=70) {
 		suggestion += "a jacket";
-	} else {
+	} else if (temp > 70 ) {
 		suggestion += "no jacket";
 	}
-
 	console.log("You are heading to a " + event + " event and should wear " + suggestion + " because it is " + temp + " degrees outside.")
 }
 
 whatDoIWear("formal", 30);
 whatDoIWear("casual", 50);
-whatDoIWear("semi-casual", 75);
+whatDoIWear("semi-formal", 75);
