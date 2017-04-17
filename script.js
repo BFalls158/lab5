6fa5dda2ede9461fd4d77ba3e0af0d79d005204a
Bryan Falls <bfalls1587@gmail.com>
2017-04-17 15:42:36 -0400

    lab 6 finished

--- script.js
+++ script.js
@@ -11,13 +11,11 @@
 // Lab 6
 
 function tempConv (temp, unit) {
-	var str = "Takes value of " + temp + unit + " and convert it to the opposite unit."
 	if (unit === "C") {
 	console.log((temp-32)*(5/9) + " C");
 	} else if (unit === "F") {
 	console.log((temp * (9/5) + 32) + " F");
 	}
-	console.log(str);
 }
 
 tempConv(212, "C");
