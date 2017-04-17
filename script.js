23a010b2f3b63bcd3507bcd52fa85cd4434b2114
Bryan Falls <bfalls1587@gmail.com>
2017-04-17 14:34:20 -0400

    temp converter

--- script.js
+++ script.js
@@ -12,9 +12,9 @@
 
 function tempConv (temp, unit) {
 	var str = "Takes value of " + temp + unit + " and convert it to the opposite unit."
-	if (unit === "F") {
+	if (unit === "C") {
 	console.log((temp-32)*(5/9) + " C");
-	} else if (unit === "C") {
+	} else if (unit === "F") {
 	console.log((temp * (9/5) + 32) + " F");
 	}
 	console.log(str);
